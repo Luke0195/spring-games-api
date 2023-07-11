cfe011ded29deaae9fd8ab1e4e0bde616ff151d1
Lucas Santos <lucas201273@gmail.com>
2023-07-10 21:53:30 -0300

    initial commit

---
+++ src/main/java/br/com/dsgames/DsgamesApplication.java
@@ -0,0 +1,13 @@
+package br.com.dsgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsgamesApplication.class, args);
+	}
+
+}
